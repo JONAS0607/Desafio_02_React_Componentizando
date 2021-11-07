6b6f33dfbbd89c24e65a98aad662d955946da427
Jonas Cabral <cabral.j@unoesc.edu.br>
2021-11-07 13:49:04 -0300

    finalizado com muita pesquisa para entender como passar dados no typescript

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,67 @@
+import { useEffect, useState } from "react";
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+import "../styles/sidebar.scss";
+
+import { api } from "../services/api";
+import { Content } from "./Content";
+
 export function SideBar() {
-  // Complete aqui
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    api.get(`movies/?Genre_id=${selectedGenreId}`).then((response) => {
+      setMovies(response.data);
+    });
+
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [selectedGenreId]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {genres.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+      <Content genreSelect={selectedGenre} allMovies={movies} />
+    </>
+  );
 }
